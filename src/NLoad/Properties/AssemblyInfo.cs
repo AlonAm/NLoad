ec3db319f1cb5967a1d515e992ac06edba53b4ab
Alon Amsalem <alon.amsalem@gmail.com>
2015-06-23 23:28:37 +0300

    nuget package desc [skip ci]

--- src/NLoad/Properties/AssemblyInfo.cs
+++ src/NLoad/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NLoad")]
-[assembly: AssemblyDescription("Simple load testing framework")]
+[assembly: AssemblyTitle("NLoad - Simple load testing framework")]
+[assembly: AssemblyDescription("A simple and friendly load testing framework for .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alon Amsalem")]
 [assembly: AssemblyProduct("NLoad")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
