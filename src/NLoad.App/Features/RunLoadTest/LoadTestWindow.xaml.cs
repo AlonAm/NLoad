6c2f49231c4dcc5b1e89a55c786438bc3394217b
alon.amsalem <alon.amsalem@gmail.com>
2015-06-28 21:19:27 +0300

    draggable window

--- src/NLoad.App/Features/RunLoadTest/LoadTestWindow.xaml.cs
+++ src/NLoad.App/Features/RunLoadTest/LoadTestWindow.xaml.cs
@@ -1,15 +1,25 @@
-﻿using System.Windows;
+﻿using System.Windows.Input;
 
 namespace NLoad.App.Features.RunLoadTest
 {
     /// <summary>
     /// Interaction logic for LoadTestWindow.xaml
     /// </summary>
-    public partial class LoadTestWindow : Window
+    public partial class LoadTestWindow
     {
         public LoadTestWindow()
         {
             InitializeComponent();
+
+            MouseDown += Window_MouseDown;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
         }
     }
 }
