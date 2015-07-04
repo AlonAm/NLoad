e29d16d2a6e9b0c18ea96fdae05eb12b565faee0
alon.amsalem <alon.amsalem@gmail.com>
2015-07-05 01:08:00 +0300

    cleanup

--- src/NLoad.App/Features/RunLoadTest/StopLoadTestCommand.cs
+++ src/NLoad.App/Features/RunLoadTest/StopLoadTestCommand.cs
@@ -5,6 +5,8 @@ namespace NLoad.App.Features.RunLoadTest
 {
     public class StopLoadTestCommand : ICommand
     {
+        //todo: use cancellation-token
+
         private readonly LoadTestViewModel _loadTestViewModel;
 
         public StopLoadTestCommand(LoadTestViewModel loadTestViewModel)
