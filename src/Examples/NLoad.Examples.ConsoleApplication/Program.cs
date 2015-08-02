76c886bdbc3d3b43ad289db3b43f63b07dc49ff3
alon.amsalem <alon.amsalem@gmail.com>
2015-08-03 00:36:31 +0300

    added Threads count

--- src/Examples/NLoad.Examples.ConsoleApplication/Program.cs
+++ src/Examples/NLoad.Examples.ConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace NLoad.Examples.ConsoleApplication
             var loadTest = NLoad.Test<MyTest>()
                                     .WithNumberOfThreads(10)
                                     .WithDurationOf(TimeSpan.FromSeconds(10))
-                                    .OnHeartbeat((s, e) => Console.WriteLine(e.Throughput))
+                                    .OnHeartbeat((s, e) => Console.WriteLine("Threads: {0} Throughput: {1}", e.TotalThreads, e.Throughput))
                                 .Build();
 
             var result = loadTest.Run();
